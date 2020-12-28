da4506816f21f0b51f89b5314bc31f426f22bda8
Jan Kammellander <janschwiderik@gmail.com>
2020-12-28 14:42:28 +0100

    📦 NEW: size for the canvas animation 🗻

--- js/homeAnimation.js
+++ js/homeAnimation.js
@@ -6,3 +6,18 @@ import { OrbitControls } from '//unpkg.com/three@0.123.0/examples/jsm/controls/O
 const renderer = new $.WebGLRenderer({ antialias: true });
 const scene = new $.Scene();
 const camera = new $.PerspectiveCamera(75, 2, .1, 1000);
+const cubeRenderTarget = new $.cubeRenderTarget(128);
+const controls = new OrbitControls(camera, renderer.domElement);
+
+window.addEventListener('resize', () => {
+    // device height & width 📱 
+    const { clientWidth, clientHeight } = renderer.domElement;
+
+    // renderer size 🗻
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(clientWidth, clientHeight, false);
+
+    // camera 🎥
+    camera.aspect = clientWidth / clientHeight;
+    camera.updateProjectMatrix();
+})
