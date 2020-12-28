18b11c261bfdc1e87995b4efea905aaced34c2da
Jan Kammellander <janschwiderik@gmail.com>
2020-12-28 11:51:33 +0100

    📦 NEW: homeAnimation for the WebGL animation 🏃‍♂️

---
+++ js/homeAnimation.js
@@ -0,0 +1,3 @@
+// Import🚚 three.js ➗
+import * as $ from '//unpkg.com/three@0.123.0/build/three.module.js'
+import { OrbitControls } from '//unpkg.com/three@0.123.0/examples/jsm/controls/OrbitControls.js'
